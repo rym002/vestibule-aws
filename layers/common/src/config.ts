8b42e97f86630f2dfe96439a631a95cb43be8281
rym002 <raymond.munian@gmail.com>
2021-01-24 00:20:43 -0500

    Load more than 10 parameters. Filter with label

--- layers/common/src/config.ts
+++ layers/common/src/config.ts
@@ -3,27 +3,50 @@ import { SSM } from 'aws-sdk';
 
 const parameterValues = {};
 const VESTIBULE_ENV = process.env['VESTIBULE_ENV'] || 'dev';
+const PARAMETER_LABEL = process.env['parameter_label'] || ''
+async function loadAllParameters(parameterPath: string, nextToken?: string): Promise<SSM.Parameter[]> {
+    console.time(`loadParameters nextToken : ${nextToken}`);
+    const ssm = new SSM()
+    const ssmResp = await ssm.getParametersByPath({
+        Path: parameterPath,
+        Recursive: true,
+        WithDecryption: true,
+        ParameterFilters: [
+            {
+                Key: 'Label',
+                Values: [
+                    PARAMETER_LABEL
+                ]
+            }
+        ],
+        NextToken: nextToken
+    }).promise();
+    console.timeEnd(`loadParameters nextToken : ${nextToken}`);
+
+    if (ssmResp.Parameters) {
+        if (ssmResp.NextToken) {
+            const next = await loadAllParameters(parameterPath, ssmResp.NextToken)
+            return ssmResp.Parameters.concat(next)
+        } else {
+            return ssmResp.Parameters
+        }
+    } else {
+        return []
+    }
+}
 
 async function loadParameters() {
     try {
-        console.time('loadParameters');
-        const ssm = new SSM()
         const parameterPath = '/vestibule/' + VESTIBULE_ENV;
-        const ssmResp = await ssm.getParametersByPath({
-            Path: parameterPath,
-            Recursive: true,
-            WithDecryption: true
-        }).promise();
-        if (ssmResp.Parameters) {
-            ssmResp.Parameters.forEach(parameter => {
-                const name = parameter.Name;
-                if (name) {
-                    const subName = name.substring(parameterPath.length + 1).split('/')
-                    const value = convertParameterValue(parameter);
-                    parseParameter(subName, parameterValues, value);
-                }
-            })
-        }
+        const parameters = await loadAllParameters(parameterPath)
+        parameters.forEach(parameter => {
+            const name = parameter.Name;
+            if (name) {
+                const subName = name.substring(parameterPath.length + 1).split('/')
+                const value = convertParameterValue(parameter);
+                parseParameter(subName, parameterValues, value);
+            }
+        })
     } finally {
         console.timeEnd('loadParameters');
     }
@@ -58,7 +81,7 @@ export async function getParameters<T>(parameterGroup: string): Promise<T> {
     if (!ret) {
         await loadParameters();
         ret = parameterValues[parameterGroup];
-        if (!ret){
+        if (!ret) {
             throw new Error('Cannot Find Property Group ' + parameterGroup);
         }
     }
